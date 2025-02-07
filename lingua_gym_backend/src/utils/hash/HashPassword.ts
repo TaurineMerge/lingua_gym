1a9acc41cffdcc34d5ab82e321b8cd6e29204f96
TaurineMerge <plotnikov12341@gmail.com>
2025-02-07 19:07:43 +0300

    feat: add password hashing utility

---
+++ lingua_gym_backend/src/utils/hash/HashPassword.ts
@@ -0,0 +1,14 @@
+import bcrypt from 'bcrypt';
+import logger from '../logger/Logger';
+
+function hashPassword(password: string): string {
+  try {
+    const saltRounds = 10;
+    return bcrypt.hashSync(password, saltRounds);
+  } catch (err) {
+    logger.error({ error: err }, 'Password hashing failed');
+    throw new Error('Password hashing failed');
+  }
+}
+
+export default hashPassword;
